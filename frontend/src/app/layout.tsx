36009c5481b0ad28a3df668a83846daddcd63293
rej-nc <rejdev24@gmail.com>
2023-10-12 19:46:47 +0200

    update: Readme

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,13 @@
-import Navbar from '@/components/custom/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import NavbarHome from '@/components/custom/NavbarHome'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Next.js 13 + Cognito',
-  description: 'Next.js 13 + Cognito + TailwindCSS + TypeScript + Shadcn, client side authentication demo',
+  title: 'Notes App Demo',
+  description: 'Notes App Demo - AWS CDK + Next.js 13',
+  other: { google: 'notranslate' }
 }
 
 export default function RootLayout({
@@ -19,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className='scroll-smooth'>
       <body>
-        {/* <Navbar /> */}
         {children}
       </body>
     </html>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,18 +7,21 @@ export default function Home() {
     <main className="flex min-h-screen flex-col justify-center items-center px-12 py-28 max-w-[1200px] mx-auto">
       <NavbarHome />
       <h1 className="text-3xl font-semibold text-center">
-        Next.js 13 + Cognito + API Gateway + Lambda + DynamoDB - Books App Demo
+        Next.js 13 + Cognito + API Gateway + Lambda + DynamoDB - Notes App Demo
       </h1>
       <div className="py-4">
         <p className="text-muted-foreground mt-4">
           This frontend application serves as a simple demonstration of how to interact with a backend built using AWS CDK.
         </p>
         <p className="text-muted-foreground mt-4">
+          This is an updated version of the <a href="https://github.com/jer-nc/fullstack-book-crud-cdk-nextjs13" target="_blank" className="text-blue-500">Fullstack Book App</a> demo. This version is owner based, meaning that each user can only see their own data and make changes to it.
+        </p>
+        <p className="text-muted-foreground mt-4">
           It performs basic CRUD operations, with API Gateway methods protected by Cognito authorizers.
           Lambda functions process requests and handle data modifications in DynamoDB.
         </p>
         <p className="text-muted-foreground mt-4">
-          You can finde the backend code <a href="https://github.com/jer-nc/books-api-crud-cdk" target="_blank" className="text-blue-500">here</a>.
+          You can find the source code <a href="https://github.com/jer-nc/fullstack-notes-api-cdk-nextjs13" target="_blank" className="text-blue-500">here</a>.
         </p>
         <div className="py-12 w-full justify-center flex">
           <Image priority width={900} height={800} src={image} style={{ width: 'auto' }} alt="next-js-cognito" />
@@ -29,11 +32,11 @@ export default function Home() {
         <div className="py-4">
           <ul className="text-muted-foreground">
             <li>- Authentication with Cognito</li>
-            <li>- Book creation</li>
-            <li>- Book modification</li>
-            <li>- Book deletion</li>
-            <li>- Listing all books</li>
-            <li>- Retrieving a book by its ID.</li>
+            <li>- Note creation</li>
+            <li>- Note modification</li>
+            <li>- Note deletion</li>
+            <li>- Listing all notes and sorted by recent (GSI)</li>
+            <li>- Retrieving a note by its ID.</li>
           </ul>
         </div>
         <div className="mt-8 p-4 bg-gray-200 rounded-md">
